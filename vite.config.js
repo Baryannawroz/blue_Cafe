d2f23d8c44ea8118f99abacf749a18648851c665
Baryan nawroz <baryannawroz1@gmail.com>
2025-07-25 01:14:50 +0300

    git return vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: true,
-        port: 5173,
-        cors: {
-            origin: 'https://bluecafe.malaygawra.com',
-            credentials: true,
-        }
-    },
     plugins: [
         laravel({
             input: ['resources/assets/js/app.js'],
