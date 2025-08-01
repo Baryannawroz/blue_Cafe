2eef3e0a61b296243ab6d8722cc17da1172ebff0
Baryan nawroz <baryannawroz1@gmail.com>
2025-08-02 00:18:04 +0300

    alter change

--- storage/framework/views/ce219ce3eb92e46132df3504a51480a1.php
+++ storage/framework/views/ce219ce3eb92e46132df3504a51480a1.php
@@ -8,54 +8,54 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title><?php echo $__env->yieldContent('title',config('app.name')); ?></title>
+    <title><?php echo $__env->yieldContent('title', config('app.name')); ?></title>
 
 
-    
-    
+
+    <?php echo app('Illuminate\Foundation\Vite')('resources/assets/js/app.js'); ?>
     <?php echo $__env->make('assets.css', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     <?php echo $__env->yieldContent('extra-css'); ?>
 </head>
 
 <body class="fixed-left">
     <div id="wrapper">
-        
+
         <?php echo $__env->make('assets.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
 
-        <?php if(auth()->guard()->guest()): ?>
-        <?php echo $__env->yieldContent('content'); ?>
+        <?php if (auth()->guard()->guest()): ?>
+            <?php echo $__env->yieldContent('content'); ?>
         <?php else: ?>
 
-        
-        <?php if (\Illuminate\Support\Facades\Blade::check('admin')): ?>
-        <?php echo $__env->make('assets.sidebar.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        <?php endif; ?>
 
-        <?php if (\Illuminate\Support\Facades\Blade::check('kitchen')): ?>
-        <?php echo $__env->make('assets.sidebar.kitchen', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        <?php endif; ?>
+            <?php if (\Illuminate\Support\Facades\Blade::check('admin')): ?>
+                <?php echo $__env->make('assets.sidebar.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php endif; ?>
 
-        <?php if (\Illuminate\Support\Facades\Blade::check('waiter')): ?>
-        <?php echo $__env->make('assets.sidebar.waiter', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        <?php endif; ?>
+            <?php if (\Illuminate\Support\Facades\Blade::check('kitchen')): ?>
+                <?php echo $__env->make('assets.sidebar.kitchen', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php endif; ?>
 
-        <?php if (\Illuminate\Support\Facades\Blade::check('manager')): ?>
-        <?php echo $__env->make('assets.sidebar.manager', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        <?php endif; ?>
+            <?php if (\Illuminate\Support\Facades\Blade::check('waiter')): ?>
+                <?php echo $__env->make('assets.sidebar.waiter', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php endif; ?>
 
-        <div class="content-page">
-            <div class="content">
-                <div class="container">
-                    
-                    <?php echo $__env->yieldContent('content'); ?>
+            <?php if (\Illuminate\Support\Facades\Blade::check('manager')): ?>
+                <?php echo $__env->make('assets.sidebar.manager', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php endif; ?>
+
+            <div class="content-page">
+                <div class="content">
+                    <div class="container">
+
+                        <?php echo $__env->yieldContent('content'); ?>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php endif; ?>
 
 
-        
+
         <?php echo $__env->make('assets.js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         <?php echo $__env->yieldContent('extra-js'); ?>
         <?php echo $__env->make('assets.show-session-message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
