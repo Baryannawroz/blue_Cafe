7e795ba4ea3bae3e05aeac8da64b7bc562267e53
Baryan nawroz <baryannawroz1@gmail.com>
2025-10-03 14:15:36 +0300

    set MySQL session timezone to Baghdad (+03:00) in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 
 
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // Force MySQL session to Baghdad (+03:00)
+        DB::statement("SET time_zone = '+03:00'");
 
 
         Blade::if('admin', function () {
